b896c75fd4f1baed50a549e63b547f61474c3c6b
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-24 17:36:00 +0200

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '54.186.63.241', user: 'deploy', roles: %w{web app db}
+server '54.202.129.207', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
